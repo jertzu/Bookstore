de6a813756295f6cb24ab897add7d14a7722b4f1
Jere Sikstus <jere@sikstus.com>
2022-09-11 22:17:47 +0300

    Initial commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,11 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	
+    private String title;
+    private String author;
+    private int year;
+    private String isbn;
+	private double price;
+    
+}
