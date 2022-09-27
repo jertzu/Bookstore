1e6553238c87f26db27d00d61dfd3b54955aa1de
Jere Sikstus <jere@sikstus.com>
2022-09-27 21:58:32 +0300

    rest

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -26,6 +28,16 @@ public class BookController {
     public String bookList(Model model) {	
         model.addAttribute("books", bookRepository.findAll());
         return "/booklist";
+    }
+    
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) bookRepository.findAll();
+    }
+    
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookRepository.findById(bookId);
     }
   
     @RequestMapping(value = "/add")
